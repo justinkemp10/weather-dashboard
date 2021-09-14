4107b603c156276cbe3c86e2725b792e68561e81
Justin Kemp <jkemp0790@gmail.com>
2021-09-14 12:52:46 -0500

    fixed recent search function in script

--- script.js
+++ script.js
@@ -51,8 +51,6 @@ function init() {
     // get weather data upon click of search
     recentSearchHistory();
     getLatLon();
-    // var recentSearchLiButton = document.querySelector(".recSearchBtn");
-    // recentSearchLiButton.addEventListener("click", getLatLon());
   });
 }
 
@@ -63,52 +61,98 @@ function recentSearchHistory() {
   $("#recentSearches").children("button").remove();
   cityArray = cityArray.splice(0, 5);
   
-  // TODO: for loop to iterate through cityArray
+  // for loop to iterate through cityArray
   for (var i = 0; i < 5; i++) {
-    // TODO: create <li> element using document.createElement & assign it to a variable
+    // create <li> element using document.createElement & assign it to a variable
     var recentSearchLi = document.createElement("button");
     recentSearchLi.classList.add('list-group-item', 'btn', 'recSearchBtn', 'name-city');
+    recentSearchLi.setAttribute('id', 'recCityName');
     recentSearchLi.setAttribute('type', 'button');
     recentSearchLi.setAttribute('value', cityArray[i])
-    // TODO: set its innerHTML to cityArray[i] value at current index
+    // set its innerHTML to cityArray[i] value at current index
     recentSearchLi.innerHTML = cityArray[i];
-    // TODO: append new <li> element to <ul> element using .appendChild
+    // append new <li> element to <ul> element using .appendChild
     recentSearchUl.appendChild(recentSearchLi); 
   }
-  // $('.name-city').on('click', newRecSearch());
 }
 
-function newRecSearch() {
-    // retrieve the name of the city that was entered in the input field
-    // var recSearchName = event.target.value;
-    // console.log('test')
-    // recentSearchLi.addEventListener('click', function () {
-    //   console.log(recSearchName);
-    //   getLatLon();
-    // }); 
-  
-    // use OpenWeather current API to get the latitude & longitude of the city
-    // var apiUrl =
-    //   "http://api.openweathermap.org/data/2.5/weather?q=" +
-    //   recSearchName +
-    //   "&appid=" +
-    //   API_KEY +
-    //   "&units=imperial";
-  
-    // var lat;
-    // var lon;
-  
-    // fetch(apiUrl)
-    //   .then(function (response) {
-    //     return response.json();
-    //   })
-    //   .then(function (res) {
-    //     lat = res.coord.lat;
-    //     lon = res.coord.lon;
-    //     console.log(res);
-    //     getWeatherData(lat, lon);
-    //   });
+function newRecSearch(event) {
+  // retrieve the name of the city that was entered in the input field
+  var recCityName = event.target.value;
+  console.log('test');
+  // use OpenWeather current API to get the latitude & longitude of the city
+  var apiUrl =
+    "http://api.openweathermap.org/data/2.5/weather?q=" +
+    recCityName +
+    "&appid=" +
+    API_KEY +
+    "&units=imperial";
 
+  var lat;
+  var lon;
+
+  fetch(apiUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (res) {
+      lat = res.coord.lat;
+      lon = res.coord.lon;
+      console.log(res);
+      var apiUrl = "http://api.openweathermap.org/data/2.5/onecall?lat=" +
+        lat +
+        "&lon=" +
+        lon +
+        "&appid=" +
+        API_KEY +
+        "&units=imperial";
+
+  fetch(apiUrl)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+        var tempValue = data.current.temp;
+        var windValue = data.current.wind_speed;
+        var humidityValue = data.current.humidity;
+        var uviValue = data.current.uvi;
+        var apiIconCurrent = data.current.weather[0].icon;
+
+        var apiIcon,tempVal,windVal,humidVal,dateVal,dailyWeatherData;
+        for (var i = 0; i < 5; i++) {
+          dailyWeatherData = data.daily[i];
+          apiIcon = dailyWeatherData.weather[0].icon;
+          tempVal = dailyWeatherData.temp.max;
+          windVal = dailyWeatherData.wind_speed;
+          humidVal = dailyWeatherData.humidity;
+          dateVal = convertEpochToGMT(dailyWeatherData.dt);
+          document.querySelector("#tempDay" + (i+1)).innerHTML = 'Temp: ' + tempVal + '&#8457';
+          document.querySelector("#windDay" + (i+1)).innerHTML = 'Wind Speed: ' + windVal;
+          document.querySelector("#humidityDay" + (i+1)).innerHTML = 'Humidity: ' + humidVal + '%';;
+          document.querySelector('#iconDay' + (i+1)).src = "http://openweathermap.org/img/wn/" + apiIcon + "@2x.png";
+          document.querySelector('#day' + (i+1)).innerHTML = dateVal;
+        }
+        if (uviValue < 5) {
+          uvIndex.classList.add('btn-success');
+          uvIndex.classList.remove('btn-danger', 'btn-warning');
+        } else if (uviValue > 4 && uviValue < 8) {
+          uvIndex.classList.add('btn-warning');
+          uvIndex.classList.remove('btn-success', 'btn-danger');
+        } else if (uviValue > 7) {
+          uvIndex.classList.add('btn-danger');
+          uvIndex.classList.remove('btn-success', 'btn-warning');
+        }
+
+        document.querySelector('#currentDate').innerHTML = convertEpochToGMT(data.current.dt);
+        currentCity.innerHTML = recCityName;
+        temp.innerHTML = 'Temp: ' + tempValue + '&#8457';
+        windSpeed.innerHTML = 'Wind Speed: ' + windValue;
+        humidity.innerHTML = 'Humidity: ' + humidityValue + '%';
+        uvIndex.innerHTML = 'UV Index: ' + uviValue;
+        document.querySelector('#icon').src = "http://openweathermap.org/img/wn/" + apiIconCurrent + "@2x.png";
+    })  
+    });
 }
 
 function getLatLon() {
@@ -183,14 +227,11 @@ function getWeatherData(lat, lon) {
           uvIndex.classList.add('btn-success');
           uvIndex.classList.remove('btn-danger', 'btn-warning');
         } else if (uviValue > 4 && uviValue < 8) {
-          // uvIndex.classList.remove("btn-success", "btn-danger");
           uvIndex.classList.add('btn-warning');
           uvIndex.classList.remove('btn-success', 'btn-danger');
         } else if (uviValue > 7) {
-          // uvIndex.classList.remove("btn-success", "btn-warning");
           uvIndex.classList.add('btn-danger');
           uvIndex.classList.remove('btn-success', 'btn-warning');
-        } else {
         }
 
         document.querySelector('#currentDate').innerHTML = convertEpochToGMT(data.current.dt);
@@ -218,7 +259,5 @@ function convertEpochToGMT(unixTime) {
   
 
 // }
-
 init();
-$('.name-city').on('click', newRecSearch());
-
+$(document.body).on('click', '.name-city', newRecSearch);
